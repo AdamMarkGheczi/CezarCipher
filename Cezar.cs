9cc98b07da2d83ee716a876ee87165adc33c3c7d
Adam Gheczi <adamark03@gmail.com>
2023-10-08 19:52:19 +0300

    first commit

---
+++ Cezar.cs
@@ -0,0 +1,55 @@
+﻿namespace Cezar
+{
+    internal class Cezar
+    {
+        private string alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+        public void SetCustomAlphabet(string alphabet)
+        {
+            this.alphabet = alphabet;
+        }
+
+        public string Encrypt(string input)
+        {
+            string output = "";
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                int index = alphabet.IndexOf(Char.ToLower(input[i]));
+                char currentChar = input[i];
+
+                if (index != -1){
+                    currentChar = alphabet[(index + 3) % alphabet.Length];
+
+                    if (char.IsUpper(input[i])) currentChar = char.ToUpper(currentChar);
+                }
+                
+                output += currentChar;
+            }
+
+            return output;
+        }
+
+        public string Decrypt(string input)
+        {
+            string output = "";
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                int index = alphabet.IndexOf(Char.ToLower(input[i]));
+                char currentChar = input[i];
+
+                if (index != -1)
+                {
+                    currentChar = alphabet[(index - 3 + alphabet.Length) % alphabet.Length];
+
+                    if (char.IsUpper(input[i])) currentChar = char.ToUpper(currentChar);
+                }
+
+                output += currentChar;
+            }
+
+            return output;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Cezar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Hello World!";
+
+            Cezar cezar = new Cezar();
+
+            Console.WriteLine(text);
+            Console.WriteLine(cezar.Encrypt(text));
+        }
+    }
+}
